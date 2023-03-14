eff68706cde7ffc5cbc6d439904c072e03f54f86
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-03-14 18:13:51 -0300

    scroll view

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -36,40 +36,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  var counter = 0; 
+  var counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home")
-      ),
+      appBar: AppBar(title: Text("Home")),
       body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: 80,
-              height: 80,
-              color: Colors.red,
-            ),
-            Container(
-              width: 80,
-              height: 80,
-              color: Colors.blue,
-            ),
-            Container(
-              width: 80,
-              height: 80,
-              color: Colors.green,
-            ),
-          ],
-        )
-      ),
+          width: double.infinity,
+          height: double.infinity,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                for (var i = 0; i < 10; i++)
+                  Container(
+                    width: double.infinity,
+                    height: 80,
+                    margin: EdgeInsets.all(8),
+                    color: Colors.red,
+                  ),
+              ],
+          ))),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
