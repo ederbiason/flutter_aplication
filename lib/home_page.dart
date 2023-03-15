4b51c3bb3b2894dda8e63fe3c37f4e39766cd70a
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-03-15 15:06:37 -0300

    example with list view builder

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -36,26 +36,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   var counter = 0;
-  final names = ['Joao', 'Maria', 'Pedro', 'Eder'];
+  final list = <String>[];
+  final controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home")),
-      body: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: ListView.builder(
-            itemCount: names.length,
-            itemBuilder: ((context, index) {
-              final name = names[index];
-              return ListTile(
-                title: Text(name),
+      body: Column(
+        children: [
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(child: TextField(controller: controller,)),
+                  IconButton(onPressed: () {
+                    final text = controller.text;
+                    setState(() {
+                      list.add(text);
+                    });
+                    controller.clear();
+                  }, icon: const Icon(Icons.add))
+                ],
+              )),
+          Expanded(
+              child: ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (context, index) {
+                final item = list[index];
+
+                return ListTile(
+                  title: Text(item),
               );
-            }),
-          )),
+            },
+          ))
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
