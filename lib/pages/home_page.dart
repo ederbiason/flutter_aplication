b05739f43e67813c4c2389ba06c6184042a15321
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-03-24 15:47:50 -0300

    feat: add flutter map

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -11,19 +13,36 @@ class HomePage extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
-            onPressed: signUserOut, icon: Icon(Icons.logout),
+            onPressed: signUserOut,
+            icon: Icon(Icons.logout),
           )
         ],
+        title: Text("Maps"),
       ),
       body: Center(
-        child: Text( 
-          "Logged In as: " + user.email!,
-          style: TextStyle(fontSize: 20),  
+          child: FlutterMap(
+            options: MapOptions(
+              center: LatLng(-23.31954553676329, -51.1712561635146),
+              zoom: 12.5,
+            ),
+            nonRotatedChildren: [
+              AttributionWidget.defaultWidget(
+                source: 'OpenStreetMap contributors',
+                onSourceTapped: null,
+              ),
+            ],
+            children: [
+              TileLayer(
+                
+                urlTemplate: 'https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+            ],
         )
       ),
     );
