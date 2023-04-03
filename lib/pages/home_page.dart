828094ecd595ea1461ab9c3a5c85f631fb78f041
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-04-03 15:29:42 -0300

    feat: report cards feed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -37,64 +37,110 @@ class _HomePageState extends State<HomePage> {
         child: Container(
           alignment: Alignment.topCenter,
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: 10),
-                Image.asset('lib/assets/images/logoImage.png'),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5),
-                  margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              SizedBox(height: 10),
+              Image.asset('lib/assets/images/logoImage.png'),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5),
+                margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+                decoration: BoxDecoration(
+                    color: Color(0xFF2C3556),
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      size: 40,
+                      color: Color(0xFF44495F),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: TextField(
+                          decoration: InputDecoration(
+                        hintText: 'Pesquisar...',
+                        hintStyle:
+                            TextStyle(fontSize: 20, color: Color(0xFF44495F)),
+                        border: InputBorder.none,
+                      )),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  padding: const EdgeInsets.all(15),
+                  height: 250,
                   decoration: BoxDecoration(
-                      color: Color(0xFF2C3556),
-                      borderRadius: BorderRadius.circular(10.0)),
-                  child: Row(
+                      color: Color(0xFF1A254E),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: FlutterMap(
+                    options: MapOptions(
+                      center: LatLng(-23.31954553676329, -51.1712561635146),
+                      zoom: 13,
+                    ),
                     children: [
-                      Icon(
-                        Icons.search,
-                        size: 40,
-                        color: Color(0xFF44495F),
+                      TileLayer(
+                        urlTemplate:
+                            'https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png',
+                        userAgentPackageName: 'com.example.app',
                       ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Expanded(
-                        child: TextField(
-                            decoration: InputDecoration(
-                          hintText: 'Pesquisar...',
-                          hintStyle:
-                              TextStyle(fontSize: 20, color: Color(0xFF44495F)),
-                          border: InputBorder.none,
-                        )),
-                      )
                     ],
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Container(
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Stack(children: [
+                  Container(
+                    width: double.infinity,
                     padding: const EdgeInsets.all(15),
-                    height: 250,
                     decoration: BoxDecoration(
                         color: Color(0xFF1A254E),
                         borderRadius: BorderRadius.circular(10)),
-                    child: FlutterMap(
-                      options: MapOptions(
-                        center: LatLng(-23.31954553676329, -51.1712561635146),
-                        zoom: 13,
-                      ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        TileLayer(
-                          urlTemplate:
-                              'https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png',
-                          userAgentPackageName: 'com.example.app',
+                        Text(
+                          "Assalto - Centro",
+                          style: TextStyle(
+                              shadows: [
+                                Shadow(
+                                    color: Color(0xFFFFFFFF),
+                                    offset: Offset(0, -5))
+                              ],
+                              decoration: TextDecoration.underline,
+                              decorationColor: Color(0xFFFFFFFF),
+                              color: Colors.transparent,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Acabei de ser assaltado no centro de Londrina. O suspeito era um homem jovem, vestindo...",
+                          style:
+                              TextStyle(fontSize: 16, color: Color(0xFFFFFFFF)),
                         ),
                       ],
                     ),
                   ),
-                ),
-              ],
-            ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Icon(
+                      Icons.chevron_right,
+                      color: Color(0xFF565A6B),
+                      size: 40,
+                    ),
+                  )
+                ]),
+              ),
+            ]),
           ),
         ),
       ),
